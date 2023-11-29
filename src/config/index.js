98eb6780d52e629c8819b696b90de21ed7c839dd
Juan Antonio <132897973+JuanDNJ@users.noreply.github.com>
2023-11-30 00:08:11 +0100

    Creando: const ALL_REGIONS, Estilos

--- src/config/index.js
+++ src/config/index.js
@@ -40,7 +40,24 @@ const CHAMPIONS_INDIVIDUAL = CDN + VERSION + "/data/{language}/champion/{name}.j
  * 
 */
 const REGIONS = "https://ddragon.leagueoflegends.com/realms/na.json";
-
+const ALL_REGIONS = {
+    BR1: "br1",
+    EUN1: "eun1",
+    EUW1: "euw1",
+    JP1: "jp1",
+    KR: "kr",
+    LA1: "lan1",
+    LA2: "lan2",
+    NA1: "na1",
+    OC1: "oc1",
+    PH2: "ph2",
+    RU: "ru",
+    SG2: "sg2",
+    TH2: "th2",
+    TR1: "tr1",
+    TW2: "tw2",
+    VN2: "vn2",
+}
 /**
  * Activos de chapoteo de campeones (Champion Splash Assets)
  * https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg
@@ -253,6 +270,7 @@ export {
     CHAMPIONS_LOADING,
     CHAMPION_SPELL,
     REGIONS,
+    ALL_REGIONS,
     ITEMS,
     ITEM_ASSETS,
     LANGUAGES,
--- src/pages/public/Authentification.jsx
+++ src/pages/public/Authentification.jsx
@@ -1,18 +1,19 @@
+import styles from "./css/forms.module.css"
 import { useStore } from "@/context/Store.ctx";
-
 import { Navigate } from 'react-router-dom';
-import { DEVELOPMENT_API_KEY, GET_SUMMONER_DATA } from "../../config";
+import { DEVELOPMENT_API_KEY, GET_SUMMONER_DATA, ALL_REGIONS } from "@/config";
+import { useEffect, useState } from "react";
 export default function Authentification() {
     const { summoner, saveSummoner } = useStore()
-
+    const [regions, setRegions] = useState([])
     if (summoner) {
         return <Navigate to="/" />
     }
 
     const handlerSubmit = (eve) => {
+        const fields = Object.fromEntries(new FormData(eve.target))
         eve.preventDefault();
-        console.log(eve.target.summoner.value)
-        fetch(GET_SUMMONER_DATA.replace("{region}", eve.target.region.value).replace("{summonerName}", eve.target.summoner.value).replace("{token}", DEVELOPMENT_API_KEY))
+        fetch(GET_SUMMONER_DATA.replace("{region}", fields.region).replace("{summonerName}", fields.summoner).replace("{token}", DEVELOPMENT_API_KEY))
             .then(res => {
 
                 if (res.status === 200) {
@@ -25,16 +26,36 @@ export default function Authentification() {
                 saveSummoner(summoner)
             })
             .catch(err => console.error(err))
+
         // saveUserName(eve.target.summoner.value)
     }
-
+    useEffect(() => {
+        setRegions(() => Object.entries(ALL_REGIONS).map(region => {
+            return {
+                value: region[0],
+                region: region[1],
+            }
+        }))
+    }, [])
+    const regionsRender = regions.map((record, keyId) => (<option key={keyId} value={record.region}>{record.value}</option>))
     return (
         <>
+
             <h2>Authentification</h2>
-            <form onSubmit={handlerSubmit}>
-                <input type="text" name="region" placeholder="Insert your region by summoner name" defaultValue="euw1" />
-                <input type="text" name="summoner" placeholder="Insert your summoner name" />
-                <button type="submit">Send</button>
+            <form onSubmit={handlerSubmit} className={styles.formSelectSummoner}>
+                <div className={styles.formControlRow}>
+                    <strong>Select Region</strong>
+                    <select name="region" defaultValue={'EUW1'}>
+                        {regionsRender}
+                    </select>
+                </div>
+                <div className={styles.formControl}>
+                    <strong>Summoner Name</strong>
+                    <input type="text" id="summoner" name="summoner" placeholder="Insert name" />
+                </div>
+                <div className={styles.formControl}>
+                    <button type="submit">Send</button>
+                </div>
             </form>
         </>
     )
